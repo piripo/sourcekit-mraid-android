d3ba00b9bf6948f3f3409a56129d7ce57ac49ff5
m5 <micah@qwerjk.com>
2017-04-25 19:35:22 -0400

    add public isloaded

--- src/src/main/java/org/nexage/sourcekit/mraid/MRAIDView.java
+++ src/src/main/java/org/nexage/sourcekit/mraid/MRAIDView.java
@@ -1495,9 +1495,10 @@ public class MRAIDView extends RelativeLayout {
             }
             return null;
         }
+    }
 
-
-
+    public boolean isLoaded(){
+        return isPageFinished;
     }
 
     /**************************************************************************
